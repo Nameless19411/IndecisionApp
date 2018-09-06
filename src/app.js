33016cb5b5f1f5a2b65b89b7dfbf4fa81abc5e71
Nameless_Developer <einsteinkerk@gmail.com>
2018-09-06 14:21:34 +0700

    export default

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
-// import { square, add } from './utils.js'
-import {isAdult, canDrink} from './person.js'
+// import subtract, { square, add } from './utils.js'
+import isSenior, {isAdult, canDrink} from './person.js'
 
 console.log('app.js is running')
 // console.log(square(4))
 // console.log(add(100,23))
-console.log(isAdult(20))
-console.log(canDrink(20))
+// console.log(subtract(100,10))
+
+let age = 21
+console.log(isAdult(age) ? `${age} is adult` : `${age} is not adult`)
+console.log(canDrink(age) ? `${age} can drink!` : `${age} cannot drink :(`)
+console.log(isSenior(age) ? `${age} is senior` : `${age} is not senior!`)
