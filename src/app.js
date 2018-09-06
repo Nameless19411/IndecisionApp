7335339558f5651ef786ab0b3c685592f292fd09
OPT ALKO <>
2018-09-07 00:57:18 +0700

    Seperate Component

--- src/app.js
+++ src/app.js
@@ -1,12 +1,5 @@
-// import subtract, { square, add } from './utils.js'
-import isSenior, {isAdult, canDrink} from './person.js'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import IndecisionApp from './components/IndecisionApp.js'
 
-console.log('app.js is running')
-// console.log(square(4))
-// console.log(add(100,23))
-// console.log(subtract(100,10))
-
-let age = 21
-console.log(isAdult(age) ? `${age} is adult` : `${age} is not adult`)
-console.log(canDrink(age) ? `${age} can drink!` : `${age} cannot drink :(`)
-console.log(isSenior(age) ? `${age} is senior` : `${age} is not senior!`)
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
